1031b6e9d28ea43f191a084b6b05f58a13484344
arronite <ary45hira2i@gmail.com>
2023-09-17 19:38:57 +0330

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+  // Target must be serverless
+  target: "serverless",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
