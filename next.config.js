c7c1aba28ba708d1f74ef39f471ed3c416e371e8
arronite <ary45hira2i@gmail.com>
2023-09-18 13:27:40 +0330

    third

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-// next.config.js
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  // Your Next.js configuration options here
+  output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
